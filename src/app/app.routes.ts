b6c495f45d78d505ddd20ec3d6a3ca218781e291
usman <usmangomezabeuov@gmail.com>
2026-04-15 22:37:17 +0200

    chore: setup tooling, json-server, models and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./core/layout/main/main').then((m) => m.Main),
+    children: [
+      {
+        path: '',
+        redirectTo: 'heroes',
+        pathMatch: 'full',
+      },
+      {
+        path: 'heroes',
+        loadChildren: () => import('./feature/heroes/heroes.routes').then((m) => m.routes),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'heroes',
+  },
+];
